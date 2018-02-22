ddeb59e10113bf95772319a8dc4aaed7ae1f5ec6
amareshadak <amareshadak88@yahoo.com>
2018-02-23 00:44:30 +0530

    Project Setup

---
+++ HrSystem/HrSystem.API/AuthContext.cs
@@ -0,0 +1,23 @@
+﻿using HrSystem.API.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HrSystem.API
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("DataContext")
+        {
+     
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+
+}
